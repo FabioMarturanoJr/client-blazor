6ed01fc7c8493fb3b8535b12a75bf4afa59f5cd4
Fabio Marturano <fabiocmjor@gmail.com>
2024-01-18 15:00:48 -0300

    chore: refactory

--- Clientes.Api/Domain/Dto/CreateClienteDto.cs
+++ Clientes.Api/Domain/Dto/CreateClienteDto.cs
@@ -1,6 +1,7 @@
 ﻿using Clientes.Api.Domain.Enum;
 using Clientes.Api.Domain.Model;
 using Clientes.Api.Extensions;
+using Clientes.Api.Service;
 using FluentValidation;
 
 namespace Clientes.Api.Domain.Dto;
@@ -44,39 +45,43 @@ public class CreateClienteDto
 
 public class CadastroClienteValidations : AbstractValidator<CreateClienteDto>
 {
-    public CadastroClienteValidations()
+    public CadastroClienteValidations(IClienteService clienteService)
     {
-        RuleFor(x => x.NomeRazao).NotEmpty().WithMessage("NomeRazao Obrigatorio").MaximumLength(150);
+        RuleFor(x => x.NomeRazao).NotEmpty().WithMessage("Nome/Razao Obrigatório").MaximumLength(150);
 
-        RuleFor(x => x.Email).NotEmpty().WithMessage("Email Obrigatorio")
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email Obrigatório")
             .Must(x => x.EmailValido()).WithMessage("Email deve ser válido")
+            .Must(x => !clienteService.ExisteEmail(x)).WithMessage("Email Já cadastrado")
             .MaximumLength(150);
 
-        RuleFor(x => x.Telefone).NotEmpty().WithMessage("Telefone Obrigatorio")
+        RuleFor(x => x.Telefone).NotEmpty().WithMessage("Telefone Obrigatório")
             .Must(x => x.RemoveCaracterEspecial()?.Length == 11).WithMessage("Telefone deve conter 11 caracteres");
 
-        RuleFor(x => x.TipoPessoa).NotEmpty().WithMessage("TipoPessoa Obrigatorio");
+        RuleFor(x => x.TipoPessoa).NotEmpty().WithMessage("TipoPessoa Obrigatório");
 
-        RuleFor(x => x.Cpf).NotEmpty().WithMessage("Cpf Obrigatorio")
+        RuleFor(x => x.Cpf).NotEmpty().WithMessage("Cpf Obrigatório")
             .Must(x => x.RemoveCaracterEspecial()?.Length == 11).WithMessage("Cpf deve conter 11 caracteres")
+            .Must(x => !clienteService.ExisteCpfCnpj(x.RemoveCaracterEspecial())).WithMessage("Cpf Já cadastrado")
             .When(x => x.Cpf != null || x.TipoPessoa == TipoPessoaEnum.Fisica.ToString("g"));
 
-        RuleFor(x => x.Cnpj).NotEmpty().WithMessage("Cnpj Obrigatorio")
+        RuleFor(x => x.Cnpj).NotEmpty().WithMessage("Cnpj Obrigatório")
             .Must(x => x.RemoveCaracterEspecial()?.Length == 14).WithMessage("Cnpj deve conter 14 caracteres")
+            .Must(x => !clienteService.ExisteCpfCnpj(x.RemoveCaracterEspecial())).WithMessage("Cnpj Já cadastrado")
             .When(x => x.Cnpj != null || x.TipoPessoa == TipoPessoaEnum.Juridica.ToString("g"));
 
-        RuleFor(x => x.InscricaoEstadual).NotEmpty().WithMessage("InscricaoEstadual Obrigatorio")
+        RuleFor(x => x.InscricaoEstadual).NotEmpty().WithMessage("InscricaoEstadual Obrigatório")
             .Must(x => x.RemoveCaracterEspecial()?.Length == 12).WithMessage("InscricaoEstadual deve conter 12 caracteres")
+            .Must(x => !clienteService.ExisteInscricaoEstadual(x)).WithMessage("Cnpj Já Inscricao Estadual")
             .When(x => !x.Isento && (x.TipoPessoa == TipoPessoaEnum.Juridica.ToString("g") || x.InscricaoEstadualPessoaFisica));
 
-        RuleFor(x => x.Genero).NotEmpty().WithMessage("Genero Obrigatorio")
+        RuleFor(x => x.Genero).NotEmpty().WithMessage("Genero Obrigatório")
             .When(x => x.TipoPessoa == TipoPessoaEnum.Fisica.ToString("g"));
 
-        RuleFor(x => x.DataNascimento).NotEmpty().WithMessage("DataNascimento Obrigatorio")
+        RuleFor(x => x.DataNascimento).NotEmpty().WithMessage("DataNascimento Obrigatório")
             .When(x => x.TipoPessoa == TipoPessoaEnum.Fisica.ToString("g"));
 
         RuleFor(x => x.Senha).NotEmpty().MinimumLength(8).MaximumLength(15);
-
-        RuleFor(x => x.ConfirmaSenha).NotEmpty().MinimumLength(8).MaximumLength(15).Equal(x => x.Senha).WithMessage("Senhas estão diferentes");
+        RuleFor(x => x.ConfirmaSenha).NotEmpty().MinimumLength(8).MaximumLength(15).
+            Equal(x => x.Senha).WithMessage("Senhas estão diferentes");
     }
 }
